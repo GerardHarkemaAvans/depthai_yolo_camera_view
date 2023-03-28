962923610192379cdeb1e57c0d27c68bdb054631
Gerard Harkema <GerardAnneHarkema@gmail.com>
2023-03-28 19:51:32 +0200

    20230328-1

--- main.py
+++ main.py
@@ -209,8 +209,8 @@ def main():
 				cv2.rectangle(frame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
 
 			cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
-			cv2.imshow("depth", depthFrameColor)
-			cv2.imshow("rgb", frame)
+			#cv2.imshow("Detected opbjects (depth)", depthFrameColor)
+			cv2.imshow("Detected opbjects (rgb)", frame)
 
 			if cv2.waitKey(1) == ord('q'):
 				break
